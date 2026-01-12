0e2e7f142b1f1e00f842c7fdd1b9f9deb9581d66
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2026-01-13 01:14:38 +0530

    feat: update addToCart function to return typed API response and add local cart management functionality

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,12 +1,33 @@
 import { api } from "@/lib/axios"
+import type { ApiResponse } from "@/types/api";
 import type { cartReqType, CartType } from "@/types/schemas/cartSchema"
 
-export const addToCart = async (product : cartReqType): Promise<CartType>=> {
+export const addToCart = async (product: cartReqType): Promise<ApiResponse<CartType | cartReqType>>=> {
     const res = await api.post("/cart/add", product);
 
     if (!res.data.success) {
         throw new Error(res.data.message)
     }
 
-    return res.data.data;
+    return res.data;
+}
+
+export function isCartType(data: CartType | cartReqType): data is CartType {
+    return (data as CartType)._id !== undefined;
+}
+
+export function addToCartLocal(product: cartReqType) {
+    const cart = JSON.parse(localStorage.getItem("cart") || "[]");
+
+    const existingItem = cart.find((item: cartReqType) => item.productId === product.productId &&
+        item.selectedWeight.value === product.selectedWeight.value &&
+        item.selectedWeight.unit === product.selectedWeight.unit);
+
+    if (existingItem) {
+        existingItem.quantity += product.quantity;
+    } else {
+        cart.push(product);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
